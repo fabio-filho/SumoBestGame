f1a7c3027581881be3120f3f613b5ee00ff03c3b
Fábio Filho <fabio.filho@filhossi.com>
2016-03-04 18:00:53 -0300

    Trabalho feito até o ID *01 .

---
+++ Sumô Best Game/Assets/Scripts/EventController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventController : MonoBehaviour {
+
+	[SerializeField]
+	GameObject player;
+
+	[SerializeField]
+	GameObject ring;
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 playerPos = new Vector2 (player.transform.position.x, player.transform.position.y);
+		Vector2 ringPos = new Vector2 (ring.transform.position.x, ring.transform.position.y);
+
+		float distance = Vector2.Distance (playerPos, ringPos);
+		//float buffer = ring.transform.localScale.x / 2 + player.transform.localScale.x;
+		float buffer = ring.GetComponent<Renderer>().bounds.size.x/2 + player.GetComponent<Renderer>().bounds.size.x / 2;
+
+		if (distance > buffer) {
+			Debug.Log ("DEAD");
+		} else {
+			Debug.Log ("NOT DEAD");
+		}
+	}
+}
---
+++ Sumô Best Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	Transform playerTransf;
+	Rigidbody2D playerBody;
+	[SerializeField]
+	float vel;
+
+	// Use this for initialization
+	void Start () {
+		playerTransf = GetComponent<Transform> ();
+		playerBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis ("Vertical");
+
+		playerBody.velocity = new Vector2 (x, y) * vel;
+
+		Vector2 moveDirection = playerBody.velocity;
+		if (moveDirection != Vector2.zero) {
+			float angle = Mathf.Atan2(moveDirection.y, moveDirection.x) * Mathf.Rad2Deg;
+			transform.rotation = Quaternion.AngleAxis(angle+270, Vector3.forward);
+		}
+
+	}
+}
